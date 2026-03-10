b3f25f72871f3a116bcd6aebb9890e5b8bd0af2c
Aditi Sahu <aditisahu152006@gmail.com>
2026-03-11 03:28:59 +0530

    UC11

--- src/UseCase11PalindromeCheckerApp.java
+++ src/UseCase11PalindromeCheckerApp.java
@@ -1,32 +1,62 @@
-class PalindromeChecker {
+import java.util.*;
 
-    // Method to check palindrome
-    public boolean checkPalindrome(String word) {
+interface PalindromeStrategy {
+    boolean isPalindrome(String word);
+}
 
-        int start = 0;
-        int end = word.length() - 1;
+// Stack based strategy
+class StackStrategy implements PalindromeStrategy {
 
-        while(start < end) {
-            if(word.charAt(start) != word.charAt(end)) {
+    public boolean isPalindrome(String word) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : word.toCharArray()) {
+            stack.push(c);
+        }
+
+        for(char c : word.toCharArray()) {
+            if(c != stack.pop()) {
                 return false;
             }
-            start++;
-            end--;
         }
 
         return true;
     }
 }
 
-public class UseCase11PalindromeCheckerApp {
+// Deque based strategy
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean isPalindrome(String word) {
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for(char c : word.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while(deque.size() > 1) {
+            if(deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+public class UseCase12PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         String word = "madam";
 
-        PalindromeChecker checker = new PalindromeChecker();
+        // Choose strategy
+        PalindromeStrategy strategy = new StackStrategy();
+        // PalindromeStrategy strategy = new DequeStrategy();
 
-        if(checker.checkPalindrome(word)) {
+        if(strategy.isPalindrome(word)) {
             System.out.println("The given string is a Palindrome");
         } else {
             System.out.println("The given string is not a Palindrome");
