6dadd3e0fdaf90427278985412fe689e4e086476
Aditi Sahu <aditisahu152006@gmail.com>
2026-03-11 03:43:26 +0530

    UC1

---
+++ src/UseCase12PalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("===================================");
+        System.out.println("        Palindrome Checker App     ");
+        System.out.println("===================================");
+
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version          : 1.0");
+
+        System.out.println("-----------------------------------");
+        System.out.println("Welcome to the Palindrome Checker!");
+        System.out.println("This application will validate");
+        System.out.println("whether a given string is a palindrome.");
+        System.out.println("-----------------------------------");
+    }
+}
