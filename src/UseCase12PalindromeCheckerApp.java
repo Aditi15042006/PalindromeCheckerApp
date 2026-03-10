6787d556d31047eb105df9036c32ee80c42d75c1
Aditi Sahu <aditisahu152006@gmail.com>
2026-03-11 03:55:13 +0530

    UC2

---
+++ src/UseCase12PalindromeCheckerApp.java
@@ -0,0 +1,20 @@
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "madam";   // hardcoded string
+        String reversed = "";
+
+        // Reverse the string
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check if palindrome
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
+    }
+}
