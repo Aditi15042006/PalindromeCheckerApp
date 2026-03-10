007b7f0679cddb7688c22bcf4789dbd9eb07ee32
Aditi Sahu <aditisahu152006@gmail.com>
2026-03-11 03:05:31 +0530

    UC6

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,34 @@
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Stack;
 
-class UseCase5PalindromeCheckerApp {
+class UseCase6PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         String word = "madam";
+
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
 
-        // Push all characters into stack
+        // Add characters to queue and stack
         for(int i = 0; i < word.length(); i++) {
-            stack.push(word.charAt(i));
+            char ch = word.charAt(i);
+            queue.add(ch);   // enqueue
+            stack.push(ch);  // push
         }
 
-        String reversed = "";
+        boolean isPalindrome = true;
 
-        // Pop characters from stack
-        while(!stack.isEmpty()) {
-            reversed = reversed + stack.pop();
+        // Compare dequeue and pop
+        while(!queue.isEmpty()) {
+            if(queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
         }
 
-        // Compare original and reversed
-        if(word.equals(reversed)) {
+        if(isPalindrome) {
             System.out.println("The given string is a Palindrome");
         } else {
             System.out.println("The given string is not a Palindrome");
